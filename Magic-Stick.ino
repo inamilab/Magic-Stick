dcc2bc5d4db27523144dcb6fb38aa22ee9c9a0e7
java311 <ponchobalandra@gmail.com>
2021-12-01 19:43:42 +0900

    bugfix: zero tilt bug fix and table volume decrease

--- Magic-Stick.ino
+++ Magic-Stick.ino
@@ -9,27 +9,34 @@
 // ===                      INITIAL SETUP                       ===
 // ================================================================
 
-void setup(){
-  Serial.begin(115200);
-  while (!Serial) delay(10); // will pause until serial console opens
-  setup_accelero();
-  startMozzi(CONTROL_RATE);
-}
-
 float impact_vel=0;
 float acc=0; float p_acc=0;
 float vel=0; float p_vel=0;
 float dist=1.0; float p_dist=1.0; float pp_dist=1.0;
 const float LEN = 2.0; //stick LEN in m
+const float LEN_MM = 2000; //stick LEN in mm
 const float H = 1.0/256.0;  // in seconds
 
-// Function when the ball rolls down
+void setup(){
+  Serial.begin(115200);
+  while (!Serial) delay(10); // will pause until serial console opens
+  setup_accelero();
+  vel = 0.0; acc= 0.0;
+  p_vel = 0.0; p_acc=0.0;
+  startMozzi(CONTROL_RATE);
+}
+
+
+// Function when the ball rolls up
 void rollingUp(float roll, float az){
-  //acc = 7.0 * sin(roll);
-  acc = az;
+  acc = 7.0 * sin(roll);
+  //acc = az;
 
   if (p_dist > LEN){
-    if (vel != 0) { impact_vel = vel; }
+    if (vel != 0) { 
+      impact_vel = vel; 
+      
+    }
     vel = 0;
   }else{
     vel = p_vel + (H * ( (p_acc + acc ) / 2.0 )   ) ;
@@ -69,12 +76,8 @@ void rollingDown(double roll, float az){
 float TILT_LIMIT = DEG_TO_RAD * 1;  
 void updateControl(){
   getAccInfo();  // gets the accelerometer info 
-
-  if ((tilt_x > 2*PI-TILT_LIMIT) && (tilt_x > TILT_LIMIT)){
-     vel = 0.0; acc= 0.0;
-     p_vel = 0.0; p_acc=0.0;
-//     Serial.print("zero ");
-  }else if ((tilt_x < 2*PI-TILT_LIMIT) && (tilt_x > PI)){
+  
+  if ((tilt_x < 2*PI-TILT_LIMIT) && (tilt_x > PI)){
     rollingDown(tilt_x, accy);
 //    Serial.print ("down ");
   }else if ((tilt_x > TILT_LIMIT) && (tilt_x < PI)){
@@ -98,6 +101,9 @@ int dd = 0;
 int index = 0;
 int updateAudio(){
   dd =  (int)(dist * 1000);
+  if ((dd < 0) || (dd > LEN_MM)){
+    return isinTable8[0];
+  }
   index = dd % 30;
   return isinTable8[index];
 }
